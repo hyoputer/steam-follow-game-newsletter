3c3746f2b6808f1093e32085b134e93c2907da02
hyoputer <ajtwlsalsdl0@gmail.com>
2020-12-15 20:30:19 +0900

    parsing gameIDs

--- src/main/java/me/hyoputer/App.java
+++ src/main/java/me/hyoputer/App.java
@@ -1,13 +1,17 @@
 package me.hyoputer;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        Jsoup.connect(Configs.STEAM_FOLLOWED_URL).get().select(".gameListRowItemName > a").eachAttr("href")
+                .stream().map(str -> Pattern.compile("\\D*").matcher(str).replaceAll("")).forEach(System.out::println);
     }
 }
