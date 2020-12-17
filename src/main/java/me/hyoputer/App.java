3d404ca74c8697441babc604f955f1a782822a9c
hyoputer <ajtwlsalsdl0@gmail.com>
2020-12-17 19:41:39 +0900

    json parsing in App

--- src/main/java/me/hyoputer/App.java
+++ src/main/java/me/hyoputer/App.java
@@ -1,9 +1,10 @@
 package me.hyoputer;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import com.google.gson.JsonArray;
 
 import org.jsoup.Jsoup;
 
@@ -17,10 +18,16 @@ public class App {
         Steam steam = new Steam(System.getenv("STEAM_USER_ID"));
         Gist gist = new Gist(System.getenv("GIST_ACCESS_KEY"));
 
+        JsonArray gists = gist.getGists();
+        StringBuilder gistId = new StringBuilder();
+        gists.forEach(json -> {
+            if (json.getAsJsonObject().get("description").getAsString().equals(System.getenv("GIST_DESCRIPTION"))) {
+                gistId.append(json.getAsJsonObject().get("id").getAsString());
+            }
+        });
+        
         List<String> gameIDs = Jsoup.connect(steam.getFollowedURL()).get().select(".gameListRowItemName > a").eachAttr("href")
                 .stream().map(str -> Pattern.compile("\\D*").matcher(str).replaceAll("")).collect(Collectors.toList());
 
-        System.out.println(gist.getGistId(System.getenv("GIST_DESCRIPTION")));
-        
     }
 }
--- src/main/java/me/hyoputer/Gist.java
+++ src/main/java/me/hyoputer/Gist.java
@@ -4,15 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonParser;
-
-import javafx.util.Pair;
 
 public class Gist {
     private String accessToken;
@@ -23,23 +19,7 @@ public class Gist {
         this.accessToken = accessToken;
     }
 
-    public String getGistId(String description) throws Exception {
-        JsonArray gists = getGists();
-
-        List<Pair<String, String>> steamGameIDs = new ArrayList<>();
-        gists.forEach(json -> steamGameIDs
-                .add(new Pair<String, String>(json.getAsJsonObject().get("description").getAsString(),
-                        json.getAsJsonObject().get("id").getAsString())));
-
-        for (Pair<String, String> pair : steamGameIDs) {
-            if (pair.getKey().equals(description))
-                return pair.getValue();
-        }
-        
-        throw new Exception();
-    }
-
-    private JsonArray getGists() throws IOException {
+    public JsonArray getGists() throws IOException {
 
         URL url = new URL(GIST_URL);
 
