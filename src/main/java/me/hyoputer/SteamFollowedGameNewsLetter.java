a462009eb54b42c5e2ddb8173acf1f4f6ba9b425
hyoputer <ajtwlsalsdl0@gmail.com>
2020-12-18 14:36:29 +0900

    get app news

--- src/main/java/me/hyoputer/SteamFollowedGameNewsLetter.java
+++ src/main/java/me/hyoputer/SteamFollowedGameNewsLetter.java
@@ -1,10 +1,12 @@
 package me.hyoputer;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -20,7 +22,7 @@ public class SteamFollowedGameNewsLetter {
         Steam steamClient = new Steam(System.getenv("STEAM_USER_ID"));
         Gist gistClient = new Gist(System.getenv("GIST_ACCESS_KEY"));
 
-        List<String> gameIDs = Jsoup.connect(steamClient.getFollowedURL()).get().select(".gameListRowItemName > a")
+        List<String> appIds = Jsoup.connect(steamClient.getFollowedURL()).get().select(".gameListRowItemName > a")
                 .eachAttr("href").stream().map(str -> Pattern.compile("\\D*").matcher(str).replaceAll(""))
                 .collect(Collectors.toList());
 
@@ -32,10 +34,32 @@ public class SteamFollowedGameNewsLetter {
             }
         });
 
-        JsonObject gistContent = JsonParser.parseString(gistClient.getGist(gistId.toString()).get("files").getAsJsonObject()
-                                            .get(System.getenv("GIST_FILE_NAME")).getAsJsonObject().get("content").getAsString())
-                                            .getAsJsonObject();
-        
+        if (gistId != null) {
+            JsonObject gistContent = JsonParser
+                    .parseString(gistClient.getGist(gistId.toString()).get("files").getAsJsonObject()
+                            .get(System.getenv("GIST_FILE_NAME")).getAsJsonObject().get("content").getAsString())
+                    .getAsJsonObject();
 
+            JsonObject newGistContent = new JsonObject();
+
+            appIds.forEach(appId -> {
+                try {
+                    JsonObject appNews = steamClient.getAppNews(appId);
+
+                    JsonElement newsIdInGist = gistContent.get(appId);
+
+                    String newsIdLatest = appNews.get("appnews").getAsJsonObject().get("newsitems").getAsJsonArray()
+                            .get(0).getAsJsonObject().get("gid").getAsString();
+
+                    if (newsIdInGist == null || !(newsIdInGist.getAsString().equals(newsIdLatest))) {
+                        // System.out.println(appId + " not equal!");
+                    }
+                    
+                    newGistContent.addProperty(appId, newsIdLatest);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
