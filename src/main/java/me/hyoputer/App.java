bb7fa6e3bd1da73e2253152e10515f6299e1fca5
hyoputer <ajtwlsalsdl0@gmail.com>
2020-12-18 13:08:16 +0900

    gist client

--- src/main/java/me/hyoputer/App.java
+++ src/main/java/me/hyoputer/App.java
@@ -16,9 +16,9 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Steam steam = new Steam(System.getenv("STEAM_USER_ID"));
-        Gist gist = new Gist(System.getenv("GIST_ACCESS_KEY"));
+        Gist gistClient = new Gist(System.getenv("GIST_ACCESS_KEY"));
 
-        JsonArray gists = gist.getGists();
+        JsonArray gists = gistClient.getGists();
         StringBuilder gistId = new StringBuilder();
         gists.forEach(json -> {
             if (json.getAsJsonObject().get("description").getAsString().equals(System.getenv("GIST_DESCRIPTION"))) {
